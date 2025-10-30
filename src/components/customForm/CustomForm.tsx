bb95d312ec4fa5441371211ff6b8232935b06c69
jherson <rodriguezpaul98@gmail.com>
2025-10-30 16:54:23 -0400

    fix: customForm zod message errors

--- src/components/customForm/CustomForm.tsx
+++ src/components/customForm/CustomForm.tsx
@@ -26,6 +26,12 @@ const CustomForm = () => {
     formState: { errors },
   } = useForm<FormValues>({
     resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
@@ -33,12 +39,41 @@ const CustomForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%' }}>
-        <InputForm name="name" control={control} label="Nombre" type="text" error={errors.name}></InputForm>
-        <InputForm name="email" control={control} label="Correo Electrónico" type="email" error={errors.email}></InputForm>
-        <InputForm name="password" control={control} label="Contraseña" type="password" error={errors.password}></InputForm>
-        <InputForm name="confirmPassword" control={control} label="Confirmar Contraseña" type="password" error={errors.confirmPassword}></InputForm>
-        <button type="submit" style={{ width: '100%', marginTop: '1rem', padding: '0.875rem' }}>Registrarse</button>
+    <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
+      <InputForm
+        name="name"
+        control={control}
+        label="Nombre"
+        type="text"
+        error={errors.name}
+      ></InputForm>
+      <InputForm
+        name="email"
+        control={control}
+        label="Correo Electrónico"
+        type="email"
+        error={errors.email}
+      ></InputForm>
+      <InputForm
+        name="password"
+        control={control}
+        label="Contraseña"
+        type="password"
+        error={errors.password}
+      ></InputForm>
+      <InputForm
+        name="confirmPassword"
+        control={control}
+        label="Confirmar Contraseña"
+        type="password"
+        error={errors.confirmPassword}
+      ></InputForm>
+      <button
+        type="submit"
+        style={{ width: "100%", marginTop: "1rem", padding: "0.875rem" }}
+      >
+        Registrarse
+      </button>
     </form>
   );
 };
